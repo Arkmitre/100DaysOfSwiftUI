4112eb42bc8cbc4cce77647845c1976e127c6441
Alexander Khorkov <Arkmitre@inbox.ru>
2024-08-15 21:17:29 +0500

    Start project 1, blank for now

---
+++ Project 1/Project 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Alexander on 15/08/2024.
+//  Copyright © 2024 Alexander Khorkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
